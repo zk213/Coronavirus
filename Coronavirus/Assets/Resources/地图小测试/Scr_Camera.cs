602a2cef105d491f86b336ed1ac9913d7f3fab79
0630Marot <60707390+0630Marot@users.noreply.github.com>
2020-02-18 05:01:38 +0800

    地图系统完成

--- Coronavirus/Assets/Resources/地图小测试/Scr_Camera.cs
+++ Coronavirus/Assets/Resources/地图小测试/Scr_Camera.cs
@@ -7,21 +7,32 @@ public class Scr_Camera : MonoBehaviour
     public float iniY = 10;
     public float iniS = 12;
 
+    public float zoomSpeed = 0.2f;
+    public float minZoom = 4;
+    public float maxZoom = 12;
+
+    float maxW = 21.5f;
+    float maxH = 12.5f;
+
+    Rigidbody2D rigid;
 
     void Awake()
     {
         transform.position = new Vector2(iniX, iniY);
+        rigid = GetComponent<Rigidbody2D>();
+        Camera.main.orthographicSize = iniS;
     }
 
     void Update()
     {
+        /*
         //Zoom out
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
 
-            if (Camera.main.orthographicSize < 12)
+            if (Camera.main.orthographicSize < maxZoom)
             {
-                Camera.main.orthographicSize += 0.2F;
+                Camera.main.orthographicSize += zoomSpeed;
             }
             else
             {
@@ -32,13 +43,33 @@ public class Scr_Camera : MonoBehaviour
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
 
-            if (Camera.main.orthographicSize > 3)
-                Camera.main.orthographicSize -= 0.2F;
+            if (Camera.main.orthographicSize > minZoom)
+                Camera.main.orthographicSize -= zoomSpeed;
         }
+        */
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit point;
+        Physics.Raycast(ray, out point, 1000);
+        Vector3 Scrolldirection = ray.GetPoint(5);
+
+        float step = speed * 20 * Time.deltaTime;
+
+        // Allows zooming in and out via the mouse wheel
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && Camera.main.orthographicSize > minZoom)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Scrolldirection, Input.GetAxis("Mouse ScrollWheel") * step);
+            Camera.main.orthographicSize -= zoomSpeed;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && Camera.main.orthographicSize < maxZoom)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Scrolldirection, Input.GetAxis("Mouse ScrollWheel") * step);
+            Camera.main.orthographicSize += zoomSpeed;
+        }
+
         float Tx = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         float Ty = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         transform.Translate(Tx, Ty, 0);//相机的移动
-
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, iniX - maxW + maxW * Camera.main.orthographicSize / iniS, iniX + maxW - maxW * Camera.main.orthographicSize / iniS), Mathf.Clamp(transform.position.y, iniY - maxH + maxH * Camera.main.orthographicSize / iniS, iniY + maxH - maxH * Camera.main.orthographicSize / iniS));
     }
 
 }
